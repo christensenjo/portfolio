076f1f72822064ffab37a2babbaf9ab7717a4b66
Joel Christensen <christensenjo14@gmail.com>
2024-02-13 21:59:53 -0700

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="flex flex-col items-center">
+        <div class="my-32 text-white text-4xl text-center font-bold">
+            <h1 class="bg-white text-fortress-200 p-4">Title</h1>
+            <!-- TODO: include icon -->
+        </div>
+
+        <div class="h-12 w-36 flex bg-fortress-200 justify-center text-center items-center">
+            <p class="font-bold text-primary">Primary</p>
+        </div>
+    </div>
+</template>
