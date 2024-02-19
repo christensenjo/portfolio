ed1de85461019acf6777ac1fcf71e49a626bbff6
Joel Christensen <christensenjo14@gmail.com>
2024-02-18 19:39:52 -0700

    Radial gradients are working

--- app.vue
+++ app.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="bg-[#F1F9FE] h-screen w-screen mx-0 px-0">
-    <NavBar
-      title="Joel Christensen"
-    />
-    <NuxtPage />
+    <div class="absolute z-0 bg-[#f1f9fe] w-full h-full">
+        <section class="h-[20vh] w-screen bg-[radial-gradient(ellipse_at_top_left,_#F23B2F_0%,#f1f9fe_70%)]"></section>
+    </div>
+    <div class="absolute z-1">
+      <NavBar
+        title="Joel Christensen"
+      />
+      <NuxtPage />
+    </div>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,5 @@
 <template>
     <div class="w-screen h-screen">
-        <div class="absolute z-0 bg-[#f1f9fe] w-full h-full">
-            <span class="absolute z-1 top-64 left-36 rounded-full w-48 h-48 bg-red-500"></span>
-        </div>
         <div class="absolute z-2 w-full h-full flex justify-center items-center">
             <p class="text-3xl text-black">asdf </p>
         </div>
