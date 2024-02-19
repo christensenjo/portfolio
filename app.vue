3d51a5264249d1e4c38aae2549b7d4756e10e6ea
Joel Christensen <christensenjo14@gmail.com>
2024-02-18 20:01:37 -0700

    position gradient circles; still need to figure out h/w on scroll

--- app.vue
+++ app.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="bg-[#F1F9FE] h-screen w-screen mx-0 px-0">
-    <div class="absolute z-0 bg-[#f1f9fe] w-full h-full">
-        <section class="h-[20vh] w-screen bg-[radial-gradient(ellipse_at_top_left,_#F23B2F_0%,#f1f9fe_70%)]"></section>
+  <div class="bg-[#F1F9FE] mx-0 px-0 overflow-y-hidden">
+    <div class="absolute z-0 bg-[#f1f9fe] w-full h-full overflow-hidden">
+      <section class="h-[33vh] w-screen bg-[radial-gradient(ellipse_at_top_left,_#F23B2F_0%,#f1f9fe_70%)]"></section>
+      <section class="absolute top-1/4 -right-1/4 h-[90vh] w-[60vw] bg-[radial-gradient(ellipse_at_center,_#7CB79D_0%,#f1f9fe_70%)]"></section>
+      <section class="absolute bottom-0 -left-1/4 h-[33vh] w-2/3 bg-[radial-gradient(ellipse_at_bottom_left,_#36B7F0_0%,#f1f9fe_70%)]"></section>
     </div>
-    <div class="absolute z-1">
+    <div class="absolute z-1 w-full h-full">
       <NavBar
         title="Joel Christensen"
       />
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="w-screen h-screen">
-        <div class="absolute z-2 w-full h-full flex justify-center items-center">
+    <div class="w-full h-full">
+        <div class="w-full h-full flex justify-center items-center">
             <p class="text-3xl text-black">asdf </p>
         </div>
     </div>
