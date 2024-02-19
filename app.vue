2f086386cd3aaff5adbea417f97849af4999cfcc
Joel Christensen <christensenjo14@gmail.com>
2024-02-19 13:54:02 -0700

    Change styles and fix radial gradients to background

--- app.vue
+++ app.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="bg-[#F1F9FE] mx-0 px-0 overflow-y-hidden">
-    <div class="absolute z-0 bg-[#f1f9fe] w-full h-full overflow-hidden">
-      <section class="h-[33vh] w-screen bg-[radial-gradient(ellipse_at_top_left,_#F23B2F_0%,#f1f9fe_70%)]"></section>
-      <section class="absolute top-1/4 -right-1/4 h-[90vh] w-[60vw] bg-[radial-gradient(ellipse_at_center,_#7CB79D_0%,#f1f9fe_70%)]"></section>
-      <section class="absolute bottom-0 -left-1/4 h-[33vh] w-2/3 bg-[radial-gradient(ellipse_at_bottom_left,_#36B7F0_0%,#f1f9fe_70%)]"></section>
+  <div class="bg-[#F1F9FE] mx-0 px-0 overflow-y-auto">
+    <div class="fixed z-0 bg-[#f1f9fe] w-full h-full overflow-hidden">
+      <section class="h-[33vh] w-screen bg-[radial-gradient(ellipse_at_top_left,_#F23B2F_0%,#f1f9fe_70%)] opacity-50"></section>
+      <section class="absolute top-1/4 -right-1/4 h-[90vh] w-[60vw] bg-[radial-gradient(ellipse_at_center,_#7CB79D_0%,#f1f9fe_70%)] opacity-80"></section>
+      <section class="absolute bottom-0 -left-1/4 h-[33vh] w-[90vw] bg-[radial-gradient(ellipse_at_bottom_left,_#36B7F0_0%,#f1f9fe_70%)]  "></section>
     </div>
-    <div class="absolute z-1 w-full h-full">
+    <div class="absolute z-10 w-full h-full">
       <NavBar
         title="Joel Christensen"
       />
