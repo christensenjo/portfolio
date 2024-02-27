878258b2764dcfa43faec90d0ad74fd08fed20e7
Joel Christensen <christensenjo14@gmail.com>
2024-02-26 18:26:18 -0700

    Add hero icon, etc

--- app.vue
+++ app.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="bg-[#F1F9FE] mx-0 px-0 overflow-y-auto">
     <div class="fixed z-0 bg-[#f1f9fe] w-full h-full overflow-hidden">
-      <section class="h-[33vh] w-screen bg-[radial-gradient(ellipse_at_top_left,_#F23B2F_0%,#f1f9fe_70%)] opacity-50"></section>
-      <section class="absolute top-1/4 -right-1/4 h-[90vh] w-[60vw] bg-[radial-gradient(ellipse_at_center,_#7CB79D_0%,#f1f9fe_70%)] opacity-80"></section>
+      <section class="h-[33vh] w-screen bg-[radial-gradient(ellipse_at_top_left,_#7CB79D_0%,#f1f9fe_70%)] opacity-50"></section>
+      <section class="absolute top-1/4 -right-1/4 h-[90vh] w-[60vw] bg-[radial-gradient(ellipse_at_center,_#F23B2F_0%,#f1f9fe_70%)] opacity-80"></section>
       <section class="absolute bottom-0 -left-1/4 h-[33vh] w-[90vw] bg-[radial-gradient(ellipse_at_bottom_left,_#36B7F0_0%,#f1f9fe_70%)]  "></section>
     </div>
     <div class="absolute z-10 w-full h-full">
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,14 @@
 <template>
-    <div class="w-full h-full">
-        <div class="w-full h-full flex justify-center items-center">
-            <p class="text-3xl text-black">asdf </p>
+    <!-- Container -->
+    <div class="w-full h-full flex flex-col justify-start lg:justify-center">
+        <!-- Hero -->
+        <div class="w-full flex flex-col justify-center items-around max-w-screen-xl mx-8 xl:mx-auto">
+            <!-- Text -->
+            <div class="flex flex-col h-fit gap-4 mt-12 2xl:mt-96">
+                <h2 class="font-extralight text-4xl md:text-6xl">Joel Christensen</h2>
+                <h1 class="font-bold text-2xl md:text-5xl 2xl:text-6xl">Full Stack Software Engineer</h1>
+            </div>
         </div>
+        <img src="~/assets/img/hero_blocks3.png" class="w-full h-64 md:h-96 lg:h-3/4 2xl:h-full 2xl:-mt-56" alt="Hero Blocks" />
     </div>
 </template>
