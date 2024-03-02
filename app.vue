bdcdedfedc3478a1227370b6b9b90e8748082b19
Joel Christensen <christensenjo14@gmail.com>
2024-03-02 11:23:11 -0700

    Move blocks footer to bottom

--- app.vue
+++ app.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="bg-[#F1F9FE] mx-0 px-0 overflow-y-auto">
     <div class="fixed z-0 bg-[#f1f9fe] w-full h-full overflow-hidden">
-      <section class="h-[33vh] w-screen bg-[radial-gradient(ellipse_at_top_left,_#7CB79D_0%,#f1f9fe_70%)] opacity-50"></section>
-      <section class="absolute top-1/4 -right-1/4 h-[90vh] w-[60vw] bg-[radial-gradient(ellipse_at_center,_#F23B2F_0%,#f1f9fe_70%)] opacity-80"></section>
-      <section class="absolute bottom-0 -left-1/4 h-[33vh] w-[90vw] bg-[radial-gradient(ellipse_at_bottom_left,_#36B7F0_0%,#f1f9fe_70%)]  "></section>
+      <section class="h-[33vh] w-screen bg-[radial-gradient(ellipse_at_top_left,_#F23B2F_0%,#f1f9fe_70%)] opacity-40 backdrop-blur-sm mix-blend-darken"></section>
+      <section class="absolute top-[100px] -right-1/3 h-[70vh] w-[90vw] lg:h-screen bg-[radial-gradient(ellipse_at_center,_#7CB79D_0%,#f1f9fe_72%)] backdrop-blur-sm mix-blend-darken"></section>
+      <section class="absolute bottom-0 h-[33vh] w-screen bg-[radial-gradient(ellipse_at_bottom_left,_#36B7F0_0%,#f1f9fe_70%)] backdrop-blur-sm mix-blend-darken"></section>
     </div>
     <div class="absolute z-10 w-full h-full">
       <NavBar
-        title="Joel Christensen"
+        title=""
       />
       <NuxtPage />
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
     <!-- Container -->
-    <div class="w-full h-full flex flex-col justify-start lg:justify-center">
+    <div class="w-full h-full flex flex-col justify-start lg:justify-center items-center">
         <!-- Hero -->
-        <div class="w-full flex flex-col justify-center items-around max-w-screen-xl mx-8 xl:mx-auto">
+        <div class="w-full h-screen flex flex-col justify-center items-around max-w-screen-xl mx-8 xl:mx-auto">
             <!-- Text -->
-            <div class="flex flex-col h-fit gap-4 mt-12 2xl:mt-96">
+            <div class="flex flex-col h-fit gap-4 mt-12">
                 <h2 class="font-extralight text-4xl md:text-6xl">Joel Christensen</h2>
                 <h1 class="font-bold text-2xl md:text-5xl 2xl:text-6xl">Full Stack Software Engineer</h1>
             </div>
         </div>
-        <img src="~/assets/img/hero_blocks3.png" class="w-full h-64 md:h-96 lg:h-3/4 2xl:h-full 2xl:-mt-56" alt="Hero Blocks" />
+        <img src="~/assets/img/hero_blocks3.png" class="w-full max-w-screen-xl mx-auto h-64 md:h-96 lg:h-1/2 2xl:h-2/3" alt="Hero Blocks" />
     </div>
 </template>
