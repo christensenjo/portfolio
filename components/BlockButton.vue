1f63c27cf31051fc700447d39647cff0801fe628
Joel Christensen <christensenjo14@gmail.com>
2024-03-03 09:45:05 -0700

    BlockButton and hero section minus image

---
+++ components/BlockButton.vue
@@ -0,0 +1,48 @@
+<script setup>
+const props = defineProps({
+    w: {
+        type: String,
+        default: "fit"
+    },
+    link: {
+        type: String,
+        default: "#"
+    },
+    color: {
+        type: String,
+        default: "tailwind"
+    }
+});
+
+const dynamicClasses = computed(() => {
+    return {
+        "w-full": props.w === "full",
+        "w-auto": props.w === "fit",
+        "w-1/2": props.w === "1/2",
+        "w-1/3": props.w === "1/3",
+        "w-2/3": props.w === "2/3",
+        "w-1/4": props.w === "1/4",
+        "w-3/4": props.w === "3/4",
+        "w-16" : props.w === "sm",
+        "w-28" : props.w === "md",
+        "w-40" : props.w === "lg",
+        "w-52" : props.w === "xl",
+        "bg-tailwind": props.color === "tailwind",
+        "bg-primary": props.color === "primary",
+        "bg-vue": props.color === "vue",
+        "bg-laravel": props.color === "laravel",
+    }
+});
+
+</script>
+
+<template>
+    <NuxtLink :to="link" class="">
+        <div 
+            class="opacity-95 backdrop-blur-sm text-white text-2xl py-2 rounded-md text-center font-medium tracking-wide"
+            :class="dynamicClasses"
+        >
+            <slot></slot>
+        </div>
+    </NuxtLink>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,21 @@
 <template>
     <!-- Container -->
-    <div class="w-full h-1/2 flex flex-col justify-start lg:justify-center items-center">
+    <div class="w-full h-2/3 flex flex-col justify-start lg:justify-start items-center mt-12">
         <Glass>
-            <div class="flex flex-col h-fit gap-4 mt-12">
+            <div class="flex flex-col gap-8 mx-16 my-24">
+                <div class="flex flex-col h-full gap-4 mt-12">
                 <h2 class="font-extralight text-4xl md:text-6xl">Joel Christensen</h2>
                 <h1 class="font-bold text-2xl md:text-5xl 2xl:text-6xl">Full Stack Software Engineer</h1>
+                </div>
+                <p class="text-2xl w-3/5 font-extralight">I’m a software engineer who loves to create for the web (and sometimes mobile). I’m currently helping small businesses get their start at <span class="font-bold">Maiden Voyage Software.</span></p>
+                <div class="flex flex-col gap-2">
+                    <BlockButton w="xl" color="tailwind">Resume</BlockButton>
+                    <BlockButton w="xl" color="vue">Contact Me</BlockButton>
+                    <BlockButton w="xl" color="laravel">Projects</BlockButton>
+                </div>
             </div>
         </Glass>
-        <img src="~/assets/img/green_block.png" class="w-48 h-48 absolute top-36 right-1/2" alt="Green Block" />
+        <img src="~/assets/img/green_block.png" class="w-72 h-72 absolute top-36 start-2/3 rotate-[7deg]" alt="Green Block" />
         <!-- Hero -->
         <!-- <div class="w-full h-screen flex flex-col justify-center items-around max-w-screen-xl mx-8 xl:mx-auto">
             
