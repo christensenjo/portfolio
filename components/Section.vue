abd74e5fee51374af6b7f4cef558366374305a88
Joel Christensen <christensenjo14@gmail.com>
2024-03-06 20:30:24 -0700

    Finish about me section styles and content

--- components/Section.vue
+++ components/Section.vue
@@ -8,7 +8,7 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="flex flex-col gap-2 md:gap-4 mb-36">
+    <div class="flex flex-col gap-2 md:gap-4">
         <h1 class="text-4xl xl:text-6xl font-bold">{{ title }}</h1>
         <slot></slot>
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
     <!-- Container -->
-    <div class="w-full h-2/3 flex flex-col justify-start lg:justify-start items-center mt-4 xl:mt-12 gap-8 xl:gap-36">
+    <div class="w-full h-2/3 flex flex-col justify-start lg:justify-start items-center mt-4 xl:mt-12 gap-8 xl:gap-24">
         <Hero />
         <Section 
             title="Projects" 
@@ -40,6 +40,22 @@
                     ]"
                 />
             </div>
+        </Section>
+        <Section
+            title="About Me" 
+            class="px-4 xl:px-12 w-full max-w-screen-xl mx-auto pb-96"
+        >
+            <Glass>
+                <div class="flex flex-col-reverse md:flex-row justify-between md:justify-around items-center">
+                    <img src="~/assets/img/capital_cutout.png" class="w-fit mx-auto md:w-fit h-96" />
+                    
+                    <div class="m-6 text-center text-xl md:text-2xl font-light h-fit flex justify-center items-center md:w-2/5 md:mx-auto">
+                        <p class="h-full my-auto">
+                            Hey there! I’m <span class="font-bold inline">Joel</span>. Since being drawn to programming as a freshman in high school, I’ve found joy in solving challenges and seeing my imagination realized in final products. I’ve worked in teams at large corporations, collaborated at a small startup, and am always engaged in my own side projects. My favorite endeavors include <span class="font-bold inline">full-stack</span> web development and <span class="font-bold inline">data visualization.</span>
+                        </p>
+                    </div>
+                </div>
+            </Glass>
         </Section>
         <!-- <img src="~/assets/img/green_block.png" class="w-72 h-72 absolute top-36 start-2/3 rotate-[7deg]" alt="Green Block" /> -->
     </div>
