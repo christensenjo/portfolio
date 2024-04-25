743af9f942485730d79bc0fa0d974515dddf53a5
Joel Christensen <christensenjo14@gmail.com>
2024-04-24 22:12:52 -0600

    Simply remove mobile menu since it's not important

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -12,7 +12,7 @@
                     <img src="/img/full_stack_logo.svg" class="mr-3 h-8 sm:h-12" alt="Logo" />
                     <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">{{ title }}</span>
                 </a>
-                <button data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
+                <button data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 text-gray-500 rounded-lg hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
                     <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
                 </button>
