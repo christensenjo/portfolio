7a2777fcc8d8dfac9f99e92bba63f4979d6e365b
Joel Christensen <christensenjo14@gmail.com>
2025-02-24 22:32:05 -0700

    Add adobe fonts

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,8 +6,40 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <link rel="stylesheet" href="https://use.typekit.net/bse3xvo.css">
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        {{-- 
+        INSTALLED FONTS
+
+        font-family: "ltc-goudy-text-pro", sans-serif;
+        font-weight: 400;
+        font-style: normal;
+
+        font-family: "ltc-goudy-text-pro-shaded", sans-serif;
+        font-weight: 400;
+        font-style: normal;
+
+
+        font-family: "dutch-mediaeval-pro", sans-serif;
+        font-weight: 400;
+        font-style: normal;
+
+        font-family: "dutch-mediaeval-pro", sans-serif;
+        font-weight: 400;
+        font-style: italic;
+
+        font-family: "dutch-mediaeval-pro", sans-serif;
+        font-weight: 700;
+        font-style: normal;
+
+        font-family: "dutch-mediaeval-pro", sans-serif;
+        font-weight: 700;
+        font-style: italic;
+
+
+        --}}
+
 
         @routes
         @vite(['resources/js/app.ts'])
