3d21647970c4dd0e6a33f40b3fd820a64adef98f
Joel Christensen <christensenjo14@gmail.com>
2026-01-10 18:02:44 -0700

    Testing Paper for desiging resources, etc

--- resources/js/layouts/app/app-landing-layout.tsx
+++ resources/js/layouts/app/app-landing-layout.tsx
@@ -10,9 +10,13 @@ export default function AppLandingLayout({ children }: AppLandingLayoutProps) {
     return (
         <div className="bg-[#F1F9FE] mx-0 px-0 overflow-y-auto">
             <div className="fixed z-0 bg-[#f1f9fe] w-full h-full overflow-hidden">
-                <section className="h-[33vh] w-screen bg-[radial-gradient(ellipse_at_top_left,_#F23B2F_0%,#f1f9fe_70%)] opacity-40 backdrop-blur-sm mix-blend-darken"></section>
+                {/* <section className="h-[33vh] w-screen bg-radial-[at_50%_75%] from-[#F23B2F] to-[#f1f9fe] backdrop-blur-sm mix-blend-darken bg-clip-content">
+                </section> */}
+
+                <section className="h-[60vh] w-screen bg-[radial-gradient(ellipse_at_top_left,_#F23B2F_0%,#f1f9fe_70%)] opacity-70 backdrop-blur-sm mix-blend-darken"></section>
+                
                 <section className="absolute top-[100px] -right-1/3 h-[70vh] w-[90vw] lg:h-screen bg-[radial-gradient(ellipse_at_center,_#7CB79D_0%,#f1f9fe_72%)] backdrop-blur-sm mix-blend-darken"></section>
-                <section className="absolute bottom-0 h-[33vh] w-screen bg-[radial-gradient(ellipse_at_bottom_left,_#36B7F0_0%,#f1f9fe_70%)] backdrop-blur-sm mix-blend-darken"></section>
+                <section className="absolute bottom-0 h-[60vh] w-screen bg-[radial-gradient(ellipse_at_bottom_left,_#36B7F0_0%,#f1f9fe_70%)] backdrop-blur-sm mix-blend-darken"></section>
             </div>
             <div className="absolute z-10 w-full h-screen scroll-smooth">
                 {/* <AppHeader breadcrumbs={breadcrumbs} /> */}
--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -20,7 +20,7 @@ export default function Welcome() {
                 <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
             </Head>
-            <div className="flex min-h-screen flex-col items-center p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:text-[#EDEDEC]">
+            <div className="flex min-h-screen flex-col items-center text-[#1b1b18] lg:justify-center dark:text-[#EDEDEC]">
                 <header className="mb-6 w-full h-fit max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl">
                     <nav className="flex items-center justify-end gap-4">
                         {/* {auth.user ? (
@@ -49,7 +49,7 @@ export default function Welcome() {
                     </nav>
                 </header>
                 <div className="flex w-full flex-1 items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0">
-                    <main className="flex w-full h-[80vh] max-w-[335px] flex-col-reverse lg:max-w-4xl lg:flex-row">
+                    <main className="flex w-full h-full flex-col-reverse lg:max-w-4xl lg:flex-row">
                         <LiquidGlassWindow className="w-full h-full" />
                     </main>
                 </div>
